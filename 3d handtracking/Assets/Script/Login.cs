7fdd63617783b3e91f65c5cc559592f4493cd308
ChillBeatz <a0976273272@gmail.com>
2023-06-23 00:27:57 +0800

    UI Update
    Start UI
    Login UI

---
+++ 3d handtracking/Assets/Script/Login.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Login : MonoBehaviour
+{   
+    int flag = 1;
+    public InputField accountField;
+    public InputField passwordField;
+
+    public Button submitButton;
+
+    public void CallLogin()
+    {
+        StartCoroutine(LoginPlayer());
+    }
+
+    IEnumerator LoginPlayer()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("php side catch word", accountField.text);         //need change php side catch word by username
+        form.AddField("php side catch word", passwordField.text);         //need change php side catch word by password
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://www.my-server.com/myform", form))        //need change php URL
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+
+                if(flag == 1)
+                {
+                    Application.LoadLevel("SampleScene");
+                }
+            }
+        }
+    }
+}
